305dff66046def4371be8afc0c221a6d928dda2e
Alperen <alperen.talaslioglu@gmail.com>
2015-08-25 00:30:33 +0300

    FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,21 @@
 /**
  * @author Alperen
- */
+ * 
+ * FizzBuzz Game implementation for division of 3 and 5
+ * 
+*/
+
+
+for(var i = 1; i <= 100; i++){
+
+	if((i % 3 == 0) && (i % 5 == 0)){
+		console.log("FizzBuzz");
+	}else if(i % 3 == 0){
+		console.log("Fizz");
+	}else if(i % 5 == 0){
+		console.log("Buzz");
+	}else{
+		console.log(i);
+	}
+	
+}
