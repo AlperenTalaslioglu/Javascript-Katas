ae5807fedc5393d6759ce8f1fdb7b76821c271ce
Alperen <alperen.talaslioglu@gmail.com>
2015-08-25 00:34:11 +0300

    Chessboard

---
+++ ConsoleChessBoard.js
@@ -0,0 +1,17 @@
+/**
+ * @author Alperen
+ * 
+ * Draw n x n chessboard to console
+ * 
+ */
+
+//Number of cells for dimension of board
+var n = 8;
+
+for(var i = 0; i < n; i++){
+	var line = "";
+	for(var j = 0; j < n; j++){
+		line += ((i+j)%2 == 0) ? "#" : " ";
+	}
+	console.log(line);
+}
