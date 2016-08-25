2c873fea58d0fbac476e3dcb4f422fc0e56204aa
Alperen <alperentalaslioglu@users.noreply.github.com>
2016-08-25 10:00:39 +0300

    Update WordReverser.js

--- WordReverser.js
+++ WordReverser.js
@@ -1,6 +1,6 @@
 
 function reverseEachWord(str) {
-    if (!isString(str) || str.length <= 0) {return 'Invalid Input';}
+    if (!isString(str) || str.length > 0) {return 'Invalid Input';}
 
     var reversedStr = '';
     var wordStack = [];
@@ -23,11 +23,10 @@ function isString(str) {
 }
 
 /*
- regex -> /[^a-zA-Z0-9]/
+ regex -> /[^a-zA-Z]/
  */
 function isAlphabetic(str) {
-    return (str.charAt(0) >= 'A' && str.charAt(0) <= 'Z')
-        || (str.charAt(0) >= 'a' && str.charAt(0) <= 'z');
+    return (str.charAt(0) >= 'A' && str.charAt(0) <= 'Z') || (str.charAt(0) >= 'a' && str.charAt(0) <= 'z');
 }
 
 var sentence = 'Thousands of requests are sent out each year, and as of 2011 about 220 proposals are returned.';
